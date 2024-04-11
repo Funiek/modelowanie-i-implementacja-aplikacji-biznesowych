7a2d329b1fed7cb731df1c754bd38c1e5ce2ba1e
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2024-04-11 22:59:13 +0200

    zad3 skonczone 2

--- zad3/src/main/java/com/example/bootdata/Console.java
+++ zad3/src/main/java/com/example/bootdata/Console.java
@@ -33,13 +33,54 @@ public class Console implements CommandLineRunner {
         u.setEnabled(true);
 
         RoleDto role = new RoleDto();
-        role.setName("Admin2");
+        role.setName("BasicUser");
 
         Set<RoleDto> roles = new HashSet<>();
         roles.add(role);
         u.setRoles(roles);
 
         userService.createUser(u);
+
+        UserDto a = new UserDto();
+        a.setLogin("admin"+System.currentTimeMillis());
+        a.setName("adminname");
+        a.setPassword("adminpass");
+        a.setEmail("adminemail");
+        a.setEnabled(true);
+
+        RoleDto role2 = new RoleDto();
+        role2.setName("BasicUser");
+
+        RoleDto role3 = new RoleDto();
+        role3.setName("Admin");
+
+        Set<RoleDto> roles2 = new HashSet<>();
+        roles2.add(role2);
+        roles2.add(role3);
+        a.setRoles(roles2);
+
+        userService.createUser(a);
+
+        UserDto m = new UserDto();
+        m.setLogin("manager"+System.currentTimeMillis());
+        m.setName("managername");
+        m.setPassword("managerpass");
+        m.setEmail("manageremail");
+        m.setEnabled(true);
+
+        RoleDto role4 = new RoleDto();
+        role4.setName("BasicUser");
+
+        RoleDto role5 = new RoleDto();
+        role5.setName("Manager");
+
+        Set<RoleDto> roles3 = new HashSet<>();
+        roles3.add(role4);
+        roles3.add(role5);
+        m.setRoles(roles3);
+
+        userService.createUser(m);
+
         userService.getAllUsers().forEach(System.out::println);
 
     }
