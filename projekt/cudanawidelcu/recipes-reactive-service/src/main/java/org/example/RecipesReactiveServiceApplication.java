d753dbbda5dc76284455a54d985e0d9ebf281336
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2024-05-31 10:23:37 +0200

    case jest taki że dodałem bazki postgres dla products i recipes do docker-compose.yml

--- projekt/cudanawidelcu/recipes-reactive-service/src/main/java/org/example/RecipesReactiveServiceApplication.java
+++ projekt/cudanawidelcu/recipes-reactive-service/src/main/java/org/example/RecipesReactiveServiceApplication.java
@@ -3,11 +3,13 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 @EnableDiscoveryClient
-public class ProductsServiceApplication {
+public class RecipesReactiveServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProductsServiceApplication.class, args);
+        SpringApplication.run(RecipesReactiveServiceApplication.class, args);
     }
 }
