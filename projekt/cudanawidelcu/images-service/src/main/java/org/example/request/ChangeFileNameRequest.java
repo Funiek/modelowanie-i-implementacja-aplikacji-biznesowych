ee8c8eaee96b52b71a3999eeee3a5bf173758822
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2024-06-06 18:08:46 +0200

    usuniecie ostatnich wersji synchronicznych mikroserwisow. calosciowe przejscie na model reaktywny w backoffice

--- projekt/cudanawidelcu/images-service/src/main/java/org/example/request/ChangeFileNameRequest.java
+++
@@ -1,9 +0,0 @@
-package org.example.request;
-
-import lombok.Data;
-
-@Data
-public class ChangeFileNameRequest {
-    String oldName;
-    String newName;
-}
--- projekt/cudanawidelcu/recipes-service/src/main/java/org/example/dto/VoteDto.java
+++
@@ -1,9 +0,0 @@
-package org.example.dto;
-
-import lombok.Data;
-
-@Data
-public class VoteDto {
-    private Long id;
-    private Double rating;
-}
