b6425e20ffc4c278dc7bd08884950f3ec05850f2
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2024-06-16 08:37:40 +0200

    dokończenie aktualizacji docker comopose i iusunięcie niepotrzebnych zdjęć z folderu img

---
+++ projekt/cudanawidelcu/sql/recipes/02-data.sql
@@ -0,0 +1,4 @@
+insert into recipe (category, description, name, created_at) values ('BREAKFAST', 'lovely PANCIAKI', 'Nalesniki', CURRENT_TIMESTAMP);
+insert into recipe (category, description, name, created_at) values ('LUNCH', 'lovely Spaghetti', 'Spaghetti', CURRENT_TIMESTAMP);
+insert into recipe (category, description, name, created_at) values ('LUNCH', 'lovely Schabowe', 'Schabowe', CURRENT_TIMESTAMP);
+insert into recipe (category, description, name, created_at) values ('DINNER', 'lovely Skyr', 'Skyr', CURRENT_TIMESTAMP);
